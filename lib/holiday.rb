848c36789c108c909d31ea08b82978c2822e17f0
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-06 22:44:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
       if holiday == :christmas 
         puts holiday.to_s.capitalize! + ":" + " #{supply.to_s.join(",")}"
       else holiday == :new_years
-        puts holiday.to_s.split.capitalize! + ":" + " #{ }"
+        puts holiday.to_s.split.capitalize! + ":" + " #{:new_years.to_s.split("_")}"
       
 
 end
